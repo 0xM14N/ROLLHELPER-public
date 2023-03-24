0ffeea9df96e2c584f85c7ead1db776bab773877
Jimmy <scriptnation222@gmail.com>
2023-03-24 21:39:16 +0100

    pricing: pins + cases

--- assets/js/deposit.js
+++ assets/js/deposit.js
@@ -155,7 +155,11 @@ function setBuffValue(item) {
 
     let rollPrice = item.querySelector('footer > div:nth-child(2) > div > cw-pretty-balance > span').innerText
 
-    if (rollPrice < 200 && !itemName.includes('Knife') && !itemName.includes('Daggers') && !itemName.includes('Gloves')&& !itemName.includes('Wraps') && !itemName.includes('Doppler')) {
+    if (rollPrice < 200 && !itemName.includes('Knife')
+        && !itemName.includes('Daggers')
+        && !itemName.includes('Gloves')
+        && !itemName.includes('Wraps')
+        && !itemName.includes('Doppler')) {
         var tbuffVal = priceInfo.buff163.starting_at.price / 0.62
     }
 
