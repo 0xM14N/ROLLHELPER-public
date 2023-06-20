7f69a0e6a5062bad2c117516a3888ecb26f4f3d4
ScriptNation <43382783+ScriptNation@users.noreply.github.com>
2023-06-20 21:21:51 +0200

    smallfix

--- assets/js/crash.js
+++ assets/js/crash.js
@@ -1,9 +1,9 @@
-// removes the maintanance banner from crash to check prices
-// Need to press F5 on the crash page
-const look4banner = setInterval(function(){
-    if (banner = document.querySelector("body > cw-root > mat-sidenav-container > " +
-        "mat-sidenav-content > div > cw-crash > cw-game-in-maintenance")){
-        clearInterval(look4banner);
-        banner.remove();
-    }
-},50)
+const banner = document.querySelector("body > cw-root > mat-sidenav-container > " +
+    "mat-sidenav-content > div > cw-crash > cw-game-in-maintenance");
+
+const lookForbanner = setInterval(function(){
+        if (banner){
+            clearInterval(look4banner);
+            banner.remove();
+        }
+},50);
