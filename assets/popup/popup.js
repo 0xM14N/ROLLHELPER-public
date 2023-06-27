3afffa13c4d3e0c5dab74d6553bc761217d28929
ScriptNation <43382783+ScriptNation@users.noreply.github.com>
2023-06-27 21:40:29 +0200

    User Interface Update

--- assets/popup/popup.js
+++ assets/popup/popup.js
@@ -1,3 +1,76 @@
-// ... code for popup here
-// ...
-// ...
+let btn = document.getElementById('saveSettings');
+let switchDepo = document.getElementById('depoSwitch');
+let switchNotify = document.getElementById('notifySwitch');
+let inputEl = document.getElementById('webhookInput');
+
+document.addEventListener('DOMContentLoaded', function () {
+    restoreOptions();
+});
+
+btn.addEventListener('click', function(){
+    let webhook_value = inputEl.value;
+
+    if (webhook_value != ''){
+        chrome.tabs.query({currentWindow: true, active: true}, function (tabs){
+            var activeTab = tabs[0];
+            chrome.tabs.sendMessage(activeTab.id, {webhook: webhook_value});
+        });
+    }
+    chrome.tabs.query({currentWindow: true, active: true}, function (tabs){
+        var activeTab = tabs[0];
+        chrome.tabs.sendMessage(activeTab.id, {update: true});
+    });
+})
+
+switchDepo.addEventListener('change', function(){
+    if(this.checked){
+        chrome.storage.sync.set({
+            switchDepoState: true
+        });
+        chrome.tabs.query({currentWindow: true, active: true}, function (tabs){
+            var activeTab = tabs[0];
+            chrome.tabs.sendMessage(activeTab.id, {switchDepoState: true});
+        });
+
+    } else{
+        chrome.storage.sync.set({
+            switchDepoState: false
+        });
+        chrome.tabs.query({currentWindow: true, active: true}, function (tabs){
+            var activeTab = tabs[0];
+            chrome.tabs.sendMessage(activeTab.id, {switchDepoState: false});
+        });
+    }
+});
+
+switchNotify.addEventListener('change', function(){
+    if(this.checked){
+        chrome.storage.sync.set({
+            switchNotifyState: true
+        });
+    } else{
+        chrome.storage.sync.set({
+            switchNotifyState: false
+        });
+    }
+})
+
+function restoreOptions(){
+    chrome.storage.sync.get(["switchDepoState"]).then((res) => {
+        switchDepo.checked = res.switchDepoState;
+    });
+
+    chrome.storage.sync.get(["switchNotifyState"]).then((res) => {
+        switchNotify.checked = res.switchNotifyState;
+    });
+
+    chrome.storage.sync.get(["switchNotifyState"]).then((res) => {
+        switchNotify.checked = res.switchNotifyState;
+    });
+
+    chrome.storage.sync.get(["webhook"]).then((res) => {
+        if(res.webhook != ''){
+            inputEl.placeholder = res.webhook
+        }
+    });
+}
